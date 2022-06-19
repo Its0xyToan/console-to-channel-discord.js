f4b51033ed19c8b57b0fcf6b572034755c45bfe8
Its0xyToan <106913620+Its0xyToan@users.noreply.github.com>
2022-06-19 11:09:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,23 @@
-const { Client, Intent } = require("discord.js")
-const { readdirSync } = require("fs")
+const Discord = require("discord.js")
 
-const client = new Client({
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES', 'DIRECT_MESSAGES', 'GUILD_PRESENCES', 'GUILD_BANS'], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-    intents: [
-
-        'GUILDS', 
-
-        'GUILD_MEMBERS', 
-
-        'GUILD_MESSAGES', 
-
-        'GUILD_MESSAGE_REACTIONS', 
-
-        'DIRECT_MESSAGES', 
-
-        'DIRECT_MESSAGE_REACTIONS', 
-
-        'DIRECT_MESSAGE_TYPING', 
-
-        'GUILD_BANS', 
-
-        'GUILD_INTEGRATIONS', 
-
-        'GUILD_INVITES', 
-
-        'GUILD_PRESENCES'
-
-    ]
-
-})
 const config = require("./config")
 
 
-client.on("ready", () => {
-    client.application.commands.set(commands.map(({ execute, ...data }) => data))
+client.on("ready", () => { //When the bot is ready
     console.log("----------------------")
     console.log("Ready with " + client.user.tag)
     console.log("----------------------")
-    client.user.setPresence({ activities: [{ name: '/help | EcoBot' }], status: 'WATCHING' });
 })
-
+/////////////////////////////////////////////////////////////////////////
 let y = process.openStdin()
 y.addListener("data", res => {
-
-let x = res.toString().trim().split(/ +/g)
-client.guilds.cache.get(config.server).channels.cache.get(config.channel).send(x.join(" "));
-console.log(x.join(" ") + " was send")
+    let x = res.toString().trim().split(/ +/g)
+    client.guilds.cache.get(config.server).channels.cache.get(config.channel).send(x.join(" "));
+    console.log(x.join(" ") + " was send")
 });
+/////////////////////////////////////////////////////////////////////////
 
 
-
-client.login(config.token)
+client.login(config.token) //Login the bot
