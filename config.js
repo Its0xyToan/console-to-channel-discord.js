41fb6edf8bebc45051e9e664728a9ea5d1411ad2
Its0xyToan <106913620+Its0xyToan@users.noreply.github.com>
2022-06-19 10:55:29 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+class config {
+    constructor() {
+        this.token = "" //The token of your Discord bot.
+        this.server = "" //The id of the server where the message gona be send.
+        this.channel = "" //The id of the channel where the message gona be send.
+    }
+}
+
+module.exports = new config()
